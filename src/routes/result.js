2f6c34328140d1c2c1943056069bfb433b13a0f8
boojin <boojin63@gmail.com>
2025-03-07 00:07:19 +0900

    수정

--- src/routes/result.js
+++ src/routes/result.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const aiController = require('../controllers/aiController');
 
 // 저장된 JSON 파일을 AI 서버로 전송하는 엔드포인트
-router.post('/analyze/:fileName', aiController.analyzeReview);
+router.post('/', aiController.analyzeReview);
 
 module.exports = router;
